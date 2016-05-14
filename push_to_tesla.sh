1993f31a840e34f89b9b5aba149cda948c011bb3
Spencer Krum <nibz@spencerkrum.com>
2016-05-13 17:37:39 -0700

    update publish script

--- push_to_tesla.sh
+++ push_to_tesla.sh
@@ -3,4 +3,6 @@
 git pull
 
 scp install_puppet.sh root@tesla.nibalizer.com:/var/www/html/install_puppet.sh
+scp install_puppet4.sh root@tesla.nibalizer.com:/var/www/html/install_puppet4.sh
 scp provision.sh root@tesla.nibalizer.com:/var/www/html/provision.sh
+ssh root@tesla.nibalizer.com 'chmod 644 /var/www/html/{install_puppet.sh,install_puppet4.sh,provision.sh}'
